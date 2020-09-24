61f6dce3e138a6b9e85ec43dca9e1cdaf32724e5
binish <binishk@bgsu.edu>
2020-09-24 15:42:23 -0400

    Add React files

---
+++ precache-manifest.65896dd7aaf5d53129b56e31a9b93920.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32661b1f8a2aa99d10a16685d4339878",
+    "url": "/index.html"
+  },
+  {
+    "revision": "309709e5edbe7b01fab7",
+    "url": "/static/css/main.7997fdf6.chunk.css"
+  },
+  {
+    "revision": "4c9c0a90d9d4d7f5443c",
+    "url": "/static/js/2.5d48bd2d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5d48bd2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "309709e5edbe7b01fab7",
+    "url": "/static/js/main.a2a0bd6a.chunk.js"
+  },
+  {
+    "revision": "0934e5a0629121a12638",
+    "url": "/static/js/runtime-main.c542aba9.js"
+  },
+  {
+    "revision": "a5cebb51b4dd059bc3795f2cc578271d",
+    "url": "/static/media/TagWithMeLogo.a5cebb51.svg"
+  },
+  {
+    "revision": "4d9162dac70f352901f73dbf2c1d5cb3",
+    "url": "/static/media/augvents.4d9162da.gif"
+  },
+  {
+    "revision": "7d61893a0abbe05e5208f66fc3ab31a0",
+    "url": "/static/media/augvents.7d61893a.png"
+  },
+  {
+    "revision": "07e1554696b64b071f3215557fa09dca",
+    "url": "/static/media/background.07e15546.svg"
+  },
+  {
+    "revision": "e9ef2de80d671623fb517d946aafcd9c",
+    "url": "/static/media/email.e9ef2de8.svg"
+  },
+  {
+    "revision": "cbbe11eee6348b90061aec3fdb474f33",
+    "url": "/static/media/github.cbbe11ee.svg"
+  },
+  {
+    "revision": "b9412d475d0055ed9c4f0b466fcc14e9",
+    "url": "/static/media/linkedin.b9412d47.svg"
+  },
+  {
+    "revision": "3d191a37703a9a05da30b77052f11e96",
+    "url": "/static/media/shellshots.3d191a37.gif"
+  },
+  {
+    "revision": "7249cc4fb006dce2083f742e22230075",
+    "url": "/static/media/shellshots.7249cc4f.png"
+  },
+  {
+    "revision": "aed8e070e5c068c32445cb31f05dd23d",
+    "url": "/static/media/twitchstitch.aed8e070.png"
+  },
+  {
+    "revision": "58c3008655d9b3b0911b9d4301eb7097",
+    "url": "/static/media/twitchstitch1.58c30086.gif"
+  },
+  {
+    "revision": "80c8c84cf9e54f3286fcd19156069aa6",
+    "url": "/static/media/twitchstitch2.80c8c84c.gif"
+  }
+]);
