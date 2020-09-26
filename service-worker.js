69caf9db520b716160621002bcda34fd22e10eb0
binish <binishk@bgsu.edu>
2020-09-26 15:27:33 -0400

    Update files

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7caf85798a1067ab99a34395dd526b61.js"
+  "/precache-manifest.d787e5bd03a8b058af11be496ad76ad3.js"
 );
 
 self.addEventListener('message', (event) => {
